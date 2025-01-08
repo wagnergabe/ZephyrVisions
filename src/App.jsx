477af4ed63eee76b9ecd0f6aa1169dd4704fb62e
unknown <gabrielswagner7@gmail.com>
2025-01-07 23:34:59 -0600

    header working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Nav from './components/Nav'
-import landingPage from './assets/landing_page_zephyr.jpg'
+import Nav from './components/Nav';
+
 
 function App() {
   return (
-<div className="p-10 xl:p-24">
+<div className="p-10 bg-black h-full w-full xl:p-24">
   <Nav/>
-  <img className = "w-full h-full"src={landingPage}/>
-  </div>
+</div>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,26 @@
-import logo from '../assets/drone_logo.jpg'
+import logo from '../assets/logo_official.png'
 import { RxHamburgerMenu } from 'react-icons/rx';
+import {useState} from 'react';
 
 const ROUTES = ["Home", "About", "Services", "Contact"];
 
 function Nav() {
-
+const [mobileMenu, setMobileMenu] = useState(false)
     return (
         <nav className="flex flex-wrap justify-between items-center">
             <a href="#">
-           <img className="w-24" src={logo} />
+           <img className="w-383 h-24" src={logo} />
            </a>
 
-           <button className=" p-2 hover:bg-slate-600 focus:ring-2 focus:ring-gray-200 rounded-lg ">
+           <button className=" p-2 text-white bg-[#11BBEF] hover:bg-black focus:ring-2 focus:ring-gray-200 rounded-lg lg:hidden" onClick={() => {setMobileMenu(!mobileMenu)}}>
             <RxHamburgerMenu size={25} />
            </button>
 
            {/* Menu Items */}
-           <div className="w-full">
-            <ul className="bg-gray-50  p-4 text-lg border rounded border-grey-100 border-grey-lg">
+           <div className={`${!mobileMenu ? "" : 'hidden'} w-full lg:w-auto lg:block`}>
+            <ul className=" lg:space-x-8 flex flex-col lg:flex-row  bg-gray-200 lg:bg-transparent lg:border-none p-4 text-lg border rounded border-grey-100 border-grey-lg">
                 {ROUTES.map((item, index) => { 
-                   return <li className={`p-2 px-3 cursor-pointer ${index === 0 ? "bg-blue-500 text-white rounded-md"  : "hover:bg-gray-100"}`} key={item}>{item}</li>
+                   return <li className={`p-2 px-3 text-black cursor-pointer ${index === 0 ? "bg-[#11BBEF] text-white rounded-md lg:bg-transparent lg:text-[#11BBEB]"  : "lg:text-white hover:bg-gray-400"}`} key={item}>{item}</li>
                 })}
             </ul>
 
